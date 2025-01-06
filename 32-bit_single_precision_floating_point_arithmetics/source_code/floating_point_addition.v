56948b775217baaf624e43fe882cf6a195cf01aa
omkar-vichare <omkar.vichare@vit.edu.in>
2025-01-06 22:21:07 +0530

    floating_point_addition.v

--- 32-bit_single_precision_floating_point_arithmetics/source_code/floating_point_addition.v
+++ 32-bit_single_precision_floating_point_arithmetics/source_code/floating_point_addition.v
@@ -1,1 +1,12 @@
+module floating_point_addition#
+(
+    parameter integer DATA_WIDTH = 32
+)
+(
+    input  [DATA_WIDTH-1:0] floating1,
+    input  [DATA_WIDTH-1:0] floating2,
 
+    output [DATA_WIDTH-1:0] floating_addition_out
+);
+
+endmodule
