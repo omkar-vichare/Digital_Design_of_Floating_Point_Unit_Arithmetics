9a92ec174d94b129bd02780e9971f5f17ba48cd5
omkar-vichare <omkar.vichare@vit.edu.in>
2025-01-08 10:30:04 +0530

    floating_point_addition.v

--- 32-bit_single_precision_floating_point_arithmetics/source_code/floating_point_addition.v
+++ 32-bit_single_precision_floating_point_arithmetics/source_code/floating_point_addition.v
@@ -1,6 +1,7 @@
 module floating_point_addition#
 (
     parameter integer DATA_WIDTH = 32,
+    parameter integer MENT_WIDTH = 23,
     parameter integer EXPO_WIDTH = 8
 )
 (
@@ -10,7 +11,11 @@ module floating_point_addition#
     output [DATA_WIDTH-1:0] floating_addition_out
 );
 
-    wire   [EXPO_WIDTH-1:0] exponent1,exponent2;
     wire                    sign1    ,sign2;    
+    wire   [EXPO_WIDTH-1:0] exponent1,exponent2;
+    wire   [MENT_WIDTH-1:0] mentissa1,mentissa2;
     
+    assign {sign1,exponent1,mentissa1} = floating1;
+    assign {sign2,exponent2,mentissa2} = floating2;
+
 endmodule
