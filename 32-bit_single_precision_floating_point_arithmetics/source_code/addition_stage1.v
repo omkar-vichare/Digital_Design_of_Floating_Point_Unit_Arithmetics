9f33a9f20af48c8eda750d8160eb2fd4ef9418f3
unknown <omkar.vichare,edu,in>
2025-01-25 09:35:12 +0530

    *.v

---
+++ 32-bit_single_precision_floating_point_arithmetics/source_code/addition_stage1.v
@@ -0,0 +1,56 @@
+module addition_stage1#
+(
+	parameter integer DATA_WIDTH = 32,
+	parameter integer MENT_WIDTH = 23,
+	parameter integer EXPO_WIDTH = 8,
+)
+(
+	//INPUT_FROM_ADDITION_TOP_MODULE
+	input  [DATA_WIDTH-1:0] floating1_in,
+	input  [DATA_WIDTH-1:0] floating2_in,
+	//INPUT_FROM_CONTROL_UNIT
+	input                   mux1_sel_in,
+	input                   mux2_sel_in,
+	input                   mux3_sel_in,
+	
+	//OUTPUT_TO_CONTROL_UNIT
+	output [EXPO_WIDTH  :0] exp_diff_out,
+    //OUTPUT_TO_STAGE2 : ALIGNING_MENTISSA
+    output [MENT_WIDTH-1:0] smaller_operand_out,
+    //OUTPUT_TO_STAGE3 : MENTISSA_ADDITION
+	output [MENT_WIDTH-1:0] bigger_operand_out,
+	//OUTPUT_TO_STAGE4 : NORMALIZER
+	output [EXPO_WIDTH-1:0] bigger_exponent_out
+);
+
+	//INTERNEDIATE_SIGNAL_FOR_2's_COMPLIMENT
+	wire   [EXPO_WIDTH  :0] twos_compliment;
+	
+	//BIT_SWIZZLING
+	assign {sign1,exponent1,mentissa1} = floating1_in;
+    assign {sign2,exponent2,mentissa2} = floating2_in;
+
+    // REASON_FOR_EXPONRNT_SUBTRACTION
+    // 1. TO_GET_MAGNITUDE_TO_PERFROM_RIGHT_SHIFT
+    // 2. TO_KNOW_WHICH_EXPONENT_IS_BIGGER
+
+    assign twos_compliment = (~exponent2) + 1'b1;
+    assign exp_diff_out    =   exponent1  + twos_compliment;
+
+    // SELECTING_APPROPRIATELY : SMALLER_AND_BIGGER_MENTISSA
+    // ASSUMPTION : EXPONENT1 > EXPONENT2 
+    // IF_ASSUMPTION_IS_CORRECT_SEL_LINES_ARE_LOGIC 1
+
+	assign bigger_operand_out  = (mux1_sel_in ? mentissa1 
+											  : mentissa2);
+	
+	assign smaller_operand_out = (mux2_sel_in ? mentissa2 
+											  : mentissa1);      
+
+	// SELECT_BIGGER_EXPONENT
+	// FOR_LATER_USE_DURING_NORMALIZATION
+
+	assign bigger_operand_out  = (mux3_sel_in ? exponent1 
+										      : exponent2);
+
+endmodule
--- 32-bit_single_precision_floating_point_arithmetics/source_code/floating_point_addition.v
+++ 32-bit_single_precision_floating_point_arithmetics/source_code/floating_point_addition.v
@@ -5,17 +5,48 @@ module floating_point_addition#
     parameter integer EXPO_WIDTH = 8
 )
 (
-    input  [DATA_WIDTH-1:0] floating1,
-    input  [DATA_WIDTH-1:0] floating2,
-
+    //32_BIT_FLOATING_INPUT
+    input  [DATA_WIDTH-1:0] floating1_in,
+    input  [DATA_WIDTH-1:0] floating2_in,
+    //32_BIT_FLOATING_OUTPUT
     output [DATA_WIDTH-1:0] floating_addition_out
 );
-
+    //WIRES_FOR_BIT_SWIZZLING_FLOATING_NUMBERS
     wire                    sign1    ,sign2;    
     wire   [EXPO_WIDTH-1:0] exponent1,exponent2;
     wire   [MENT_WIDTH-1:0] mentissa1,mentissa2;
     
-    assign {sign1,exponent1,mentissa1} = floating1;
-    assign {sign2,exponent2,mentissa2} = floating2;
+    //WIRES_FOR_STAGE1 : EXPONENT_COMPARISION
+    wire    
+
+
+    //BIT_SWIZZLING
+    assign {sign1,exponent1,mentissa1} = floating1_in;
+    assign {sign2,exponent2,mentissa2} = floating2_in;
+
+    //STAGE1 : EXPONENT_COMPARISION
+    addition_stage1 stage1
+        (
+         .floating1_in        (floating1_in),    // FROM_TOP
+         .floating2_in        (floating2_in),    // FROM_TOP
+         .mux1_sel_in         (mux1_sel),        // FROM_CONTROL
+         .mux2_sel_in         (mux2_sel),        // FROM_CONTROL
+         .mux3_sel_in         (mux3_sel),        // FROM_CONTROL
+         .exp_diff_out        (exp_diff),        // TO_CONTROL
+         .bigger_operand_out  (bigger_operand),  // TO_STAGE3
+         .smaller_operand_out (smaller_operand), // TO_STAGE2
+         .bigger_exponent_out (bigger_exponent), // TO_STAGE4
+        );
+
+    //CONTROL_UNIT_FOR_FLOATING_ADDITION
+    addition_control_unit control_unit
+        (
+         .exp_diff_out    (exp_diff),            // FROM_STAGE1
+        //WARNING : CREATING_COMBINATIONAL_LOOP
+         .mux1_sel_out    (mux1_sel),            // TO_STAGE1
+         .mux2_sel_out    (mux2_sel),            // TO_STAGE1
+         .mux3_sel_out    (mux3_sel),            // TO_STAGE1
+         .
+        );
 
 endmodule
